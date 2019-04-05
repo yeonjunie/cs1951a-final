af48fa1722a95f9235f567516ee097822eb60447
jessicadai <jeska.dai@gmail.com>
2019-04-05 00:29:05 -0400

    cast to np array

--- data_crdc/crdc_preprocess.py
+++ data_crdc/crdc_preprocess.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 read_data = []
 with open('/users/amypu/documents/Data_Files_and_Layouts/CRDC 2015-16 School Data.csv', 'r') as datafile:
 	for line in datafile:
@@ -11,3 +13,5 @@ for line in read_data:
 	# only add to processed data if line[18] == Yes and line[19] == Yes and line[20] == Yes and line[21] == Yes
 	if (line[18] == "Yes") and (line[19] == "Yes") and (line[20] == "Yes") and (line[21] == "Yes"):
 		processed_data.append(line)
+
+print(np.array(processed_data).shape)
