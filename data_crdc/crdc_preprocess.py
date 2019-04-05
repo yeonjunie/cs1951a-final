b0b3e024cbc09d2d12ebd774939ff427d85b54bc
Amy Pu <amy_pu@brown.edu>
2019-04-05 01:45:06 -0400

    added in id of district and school

--- data_crdc/crdc_preprocess.py
+++ data_crdc/crdc_preprocess.py
@@ -5,9 +5,16 @@ with open('/users/amypu/documents/Data_Files_and_Layouts/CRDC 2015-16 School Dat
 
 processed_data = []
 
-
 for line in read_data:
 
 	# only add to processed data if line[18] == Yes and line[19] == Yes and line[20] == Yes and line[21] == Yes
 	if (line[18] == "Yes") and (line[19] == "Yes") and (line[20] == "Yes") and (line[21] == "Yes"):
-		processed_data.append(line)
+		# 7 digit district id
+		district_id = line[2]
+		# 5 digit school id
+		school_id = line[4]
+		# gifted and talented indicator
+		g_and_t = line[147]
+
+		processed_line = line[2] + line[4] + line[147:]
+		processed_data.append(processed_line)
